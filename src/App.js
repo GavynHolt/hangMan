90ea41a24e593c71d0233e8ef7ca1a45c7d358a7
Gavyn Holt <gavynholt@gmail.com>
2021-07-23 14:30:40 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+
+const wordArray = ['Bear', 'Elephant', 'Aligator', 'Wasp', 'Bumble Bee'];
 
 function App() {
+  const [wordToDisplay, setWordToDisplay] = useState('');
+
+  const startGame = () => {
+    const randomIdx = Math.floor(Math.random() * wordArray.length);
+    setWordToDisplay(wordArray[randomIdx]);
+    console.log(wordArray[randomIdx]);
+  };
+
+  useEffect(() => {}, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <h1>Project 3</h1>
+      <button onClick={startGame}>Start Game</button>
     </div>
   );
 }
