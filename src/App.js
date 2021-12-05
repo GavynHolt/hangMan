9fc2ed83c98ab261f37b97ec567e410d12e85dcd
Gavyn Holt <gavynholt@gmail.com>
2021-12-05 15:24:15 -0500

    fix: React import statements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { React } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
